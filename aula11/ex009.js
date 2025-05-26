ae510d2d265d8b059a263936790f155972ac3fc3
CaueDEV <cauevitor52@gmail.com>
2025-05-26 19:56:53 -0300

    javascript

---
+++ aula11/ex009.js
@@ -0,0 +1,7 @@
+var pais = 'coreia'
+console.log(`Vivendo em ${pais}`)
+if (pais == 'coreia') {
+    console.log('Você é Coreano')
+} else {
+    console.log('Você é Estrangeiro')
+}
---
+++ aula12/ex012.js
@@ -0,0 +1,9 @@
+var idade = 22
+console.log(`Você tem idade ${idade} anos`)
+if (idade < 16) {
+    console.log('NAO VOTA!')
+} else if (idade < 18 || idade > 65) {
+        console.log('VOTO OPCIONAL')
+} else {
+    console.log('VOTO OBRIGATORIO')
+}
